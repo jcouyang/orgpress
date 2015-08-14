c28797ae2e8034f69927836770fc7ffad1e1875e
Jichao Ouyang <oyanglulu@gmail.com>
2015-08-14 13:19:47 +0800

    add my source

--- ci/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b gh-pages public
-wget https://github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/archive/source.zip && unzip source.zip
-cask exec emacs --batch -l emacs.el -f org-publish-all
-# run it again for archive.org
-cask exec emacs --batch -l emacs.el -f org-publish-all
--- ci/push.sh
+++
@@ -1,4 +0,0 @@
-cd public
-git add .
-git commit -m "publi:ship: by [OrgPress](https://github.com/jcouyang/orgpress)"
-git push origin gh-pages
