465db0298272e4ca166d1224890b6053d91706c2
Jichao Ouyang <oyanglulu@gmail.com>
2015-08-14 14:37:42 +0800

    fix source folder wrong name

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b gh-pages public
 
-USERNAME=${$USERNAME:=$CIRCLE_PROJECT_USERNAME}
-REPONAME=${$REPONAME:=$CIRCLE_PROJECT_REPONAME}
-wget https://github.com/$USERNAME/$REPONAME/archive/source.zip && unzip source.zip    
+USERNAME=${USERNAME:=$CIRCLE_PROJECT_USERNAME}
+REPONAME=${REPONAME:=$CIRCLE_PROJECT_REPONAME}
+wget https://github.com/$USERNAME/$REPONAME/archive/source.zip source.zip
+unzip -jo source.zip -d source
 
 cask exec emacs --batch -l emacs.el -f org-publish-all
 # run it again for archive.org
