5063bf39022b5e1ef29f8b76167d2d220356732b
Jichao Ouyang <oyanglulu@gmail.com>
2015-08-14 13:51:06 +0800

    configurable source repo

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b gh-pages public
-wget https://github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/archive/source.zip && unzip source.zip
+
+USERNAME=${$USERNAME:=$CIRCLE_PROJECT_USERNAME}
+REPONAME=${$REPONAME:=$CIRCLE_PROJECT_REPONAME}
+wget https://github.com/$USERNAME/$REPONAME/archive/source.zip && unzip source.zip    
+
 cask exec emacs --batch -l emacs.el -f org-publish-all
 # run it again for archive.org
 cask exec emacs --batch -l emacs.el -f org-publish-all
