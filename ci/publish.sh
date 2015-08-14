732f209040910fe67c3407e80b86ce9033c8de1b
Jichao Ouyang <oyanglulu@gmail.com>
2015-08-14 15:20:46 +0800

    wrong wget args

--- ci/publish.sh
+++ ci/publish.sh
@@ -6,8 +6,10 @@ BRANCH=${BRANCH:=source}
 
 git clone git@github.com:$USERNAME/$REPONAME.git -b gh-pages public
 
-wget https://github.com/$USERNAME/$REPONAME/archive/$BRANCH.zip source.zip
-unzip -jo source.zip -d source
+wget https://github.com/$USERNAME/$REPONAME/archive/$BRANCH.zip -O source.zip
+unzip source.zip
+
+mv $REPONAME-$BRANCH source
 
 cask exec emacs --batch -l emacs.el -f org-publish-all
 # run it again for archive.org
--- ci/push.sh
+++ ci/push.sh
@@ -1,4 +1,6 @@
 cd public
+git config --global user.email "$EMAIL"
+git config --global user.name "$USERNAME"
 git add .
 git commit -m "publi:ship: by [OrgPress](https://github.com/jcouyang/orgpress)"
 git push origin gh-pages
