4503802c6644abecb5a6270c3d9841fe4125031a
Jichao Ouyang <oyanglulu@gmail.com>
2015-08-14 14:55:48 +0800

    link to new emacs

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b gh-pages public
 
 USERNAME=${USERNAME:=$CIRCLE_PROJECT_USERNAME}
 REPONAME=${REPONAME:=$CIRCLE_PROJECT_REPONAME}
-wget https://github.com/$USERNAME/$REPONAME/archive/source.zip source.zip
+BRANCH=${BRANCH:=source}
+
+git clone git@github.com:$USERNAME/$REPONAME.git -b gh-pages public
+
+wget https://github.com/$USERNAME/$REPONAME/archive/$BRANCH.zip source.zip
 unzip -jo source.zip -d source
 
 cask exec emacs --batch -l emacs.el -f org-publish-all
